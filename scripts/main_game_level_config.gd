db628f086b1395c16234c1fa61a3d1a7e09b5077
ggestein <ggestein@outlook.com>
2026-07-26 23:08:12 +0800

    edit level config 13~17

--- scripts/main_game_level_config.gd
+++ scripts/main_game_level_config.gd
@@ -599,4 +599,307 @@ static func create_all_level_configs() -> Array[LevelConfigEntry]:
 	config_entry.level_theme_config = level_theme_config
 	result.append(config_entry)
 	
+
+	# LEVEL 13
+	level_config = CoreLevelConfig.LevelConfig.new()
+	level_config.obstacles = [
+		Vector2i(3, 0),
+		Vector2i(1, 1),
+		Vector2i(1, 2),
+		Vector2i(3, 2),
+		Vector2i(0, 3),
+		Vector2i(1, 3),
+		Vector2i(2, 3),
+		Vector2i(3, 3),
+		Vector2i(4, 3),
+		Vector2i(6, 3),
+		Vector2i(1, 4),
+		Vector2i(3, 4),
+		Vector2i(1, 5),
+		Vector2i(3, 6),
+	]
+	level_config.range = Rect2i(0, 0, 7, 7)
+	level_config.goals = [
+		Vector2i(0, 1),
+		Vector2i(0, 4),
+	]
+	level_config.characters = []
+	char_config = CoreLevelConfig.CharacterConfig.new()
+	char_config.inst_cls = CoreGameplay.CharacterClass.WARRIOR
+	char_config.inst_pos = Vector2i(2, 4)
+	char_config.inst_face = 1
+	level_config.characters.append(char_config)
+	char_config = CoreLevelConfig.CharacterConfig.new()
+	char_config.inst_cls = CoreGameplay.CharacterClass.MAGE
+	char_config.inst_pos = Vector2i(2, 2)
+	char_config.inst_face = 1
+	level_config.characters.append(char_config)
+	level_config.boxes = [
+		Vector2i(0, 2),
+		Vector2i(0, 5),
+	]
+	level_config.pads = [
+		Vector2i(5, 1),
+		Vector2i(5, 5),
+	]
+	door_config = CoreLevelConfig.DoorConfig.new();
+	door_config.position = Vector2i(5, 3)
+	door_config.need_pads_indices = [0, 1]
+	level_config.doors.append(door_config)
+	level_theme_config = LevelTheme.LevelThemeConfig.new()
+	config_entry = LevelConfigEntry.new()
+	config_entry.name = "LEVEL 13"
+	config_entry.level_config = level_config
+	config_entry.level_theme_config = level_theme_config
+	result.append(config_entry)
+	
+
+	# LEVEL 14
+	level_config = CoreLevelConfig.LevelConfig.new()
+	level_config.obstacles = [
+		Vector2i(3, 0),
+		Vector2i(4, 0),
+		Vector2i(5, 0),
+		Vector2i(9, 0),
+		Vector2i(5, 1),
+		Vector2i(6, 1),
+		Vector2i(7, 1),
+		Vector2i(9, 1),
+		Vector2i(3, 2),
+		Vector2i(5, 2),
+		Vector2i(0, 3),
+		Vector2i(1, 3),
+		Vector2i(2, 3),
+		Vector2i(3, 3),
+		Vector2i(3, 4),
+		Vector2i(5, 4),
+		Vector2i(5, 5),
+		Vector2i(6, 5),
+		Vector2i(7, 5),
+		Vector2i(9, 5),
+		Vector2i(3, 6),
+		Vector2i(4, 6),
+		Vector2i(5, 6),
+		Vector2i(9, 6),
+	]
+	level_config.range = Rect2i(0, 0, 10, 7)
+	level_config.goals = [
+		Vector2i(6, 0),
+		Vector2i(6, 6),
+	]
+	level_config.characters = []
+	char_config = CoreLevelConfig.CharacterConfig.new()
+	char_config.inst_cls = CoreGameplay.CharacterClass.WARRIOR
+	char_config.inst_pos = Vector2i(1, 5)
+	char_config.inst_face = 1
+	level_config.characters.append(char_config)
+	char_config = CoreLevelConfig.CharacterConfig.new()
+	char_config.inst_cls = CoreGameplay.CharacterClass.THIEF
+	char_config.inst_pos = Vector2i(1, 1)
+	char_config.inst_face = 1
+	level_config.characters.append(char_config)
+	level_config.boxes = [
+		Vector2i(3, 5),
+		Vector2i(6, 3),
+	]
+	level_config.pads = [
+		Vector2i(7, 2),
+		Vector2i(7, 4),
+	]
+	door_config = CoreLevelConfig.DoorConfig.new();
+	door_config.position = Vector2i(8, 1)
+	door_config.need_pads_indices = [0, 1]
+	level_config.doors.append(door_config)
+	door_config = CoreLevelConfig.DoorConfig.new();
+	door_config.position = Vector2i(8, 5)
+	door_config.need_pads_indices = [0, 1]
+	level_config.doors.append(door_config)
+	level_theme_config = LevelTheme.LevelThemeConfig.new()
+	config_entry = LevelConfigEntry.new()
+	config_entry.name = "LEVEL 14"
+	config_entry.level_config = level_config
+	config_entry.level_theme_config = level_theme_config
+	result.append(config_entry)
+	
+
+	# LEVEL 15
+	level_config = CoreLevelConfig.LevelConfig.new()
+	level_config.obstacles = [
+		Vector2i(5, 0),
+		Vector2i(8, 0),
+		Vector2i(0, 1),
+		Vector2i(1, 1),
+		Vector2i(2, 1),
+		Vector2i(3, 1),
+		Vector2i(4, 1),
+		Vector2i(7, 1),
+		Vector2i(8, 1),
+		Vector2i(9, 1),
+		Vector2i(10, 1),
+		Vector2i(2, 2),
+		Vector2i(9, 3),
+		Vector2i(2, 4),
+		Vector2i(0, 5),
+		Vector2i(1, 5),
+		Vector2i(2, 5),
+		Vector2i(3, 5),
+		Vector2i(4, 5),
+		Vector2i(5, 5),
+		Vector2i(7, 5),
+		Vector2i(8, 5),
+		Vector2i(9, 5),
+		Vector2i(10, 5),
+		Vector2i(3, 6),
+		Vector2i(9, 6),
+	]
+	level_config.range = Rect2i(0, 0, 11, 7)
+	level_config.goals = [
+		Vector2i(0, 2),
+		Vector2i(0, 4),
+	]
+	level_config.characters = []
+	char_config = CoreLevelConfig.CharacterConfig.new()
+	char_config.inst_cls = CoreGameplay.CharacterClass.MAGE
+	char_config.inst_pos = Vector2i(4, 6)
+	char_config.inst_face = 1
+	level_config.characters.append(char_config)
+	char_config = CoreLevelConfig.CharacterConfig.new()
+	char_config.inst_cls = CoreGameplay.CharacterClass.THIEF
+	char_config.inst_pos = Vector2i(3, 3)
+	char_config.inst_face = 1
+	level_config.characters.append(char_config)
+	level_config.boxes = [
+		Vector2i(8, 6),
+		Vector2i(7, 0),
+	]
+	level_config.pads = [
+		Vector2i(4, 3),
+		Vector2i(10, 3),
+	]
+	door_config = CoreLevelConfig.DoorConfig.new();
+	door_config.position = Vector2i(2, 3)
+	door_config.need_pads_indices = [0, 1]
+	level_config.doors.append(door_config)
+	level_theme_config = LevelTheme.LevelThemeConfig.new()
+	config_entry = LevelConfigEntry.new()
+	config_entry.name = "LEVEL 15"
+	config_entry.level_config = level_config
+	config_entry.level_theme_config = level_theme_config
+	result.append(config_entry)
+	
+
+	# LEVEL 16
+	level_config = CoreLevelConfig.LevelConfig.new()
+	level_config.obstacles = [
+		Vector2i(3, 0),
+		Vector2i(4, 0),
+		Vector2i(6, 0),
+		Vector2i(7, 0),
+		Vector2i(10, 0),
+		Vector2i(3, 2),
+		Vector2i(4, 2),
+		Vector2i(6, 2),
+		Vector2i(7, 2),
+		Vector2i(10, 2),
+	]
+	level_config.range = Rect2i(0, 0, 11, 3)
+	level_config.goals = [
+		Vector2i(9, 0),
+		Vector2i(9, 2),
+	]
+	level_config.characters = []
+	char_config = CoreLevelConfig.CharacterConfig.new()
+	char_config.inst_cls = CoreGameplay.CharacterClass.WARRIOR
+	char_config.inst_pos = Vector2i(5, 2)
+	char_config.inst_face = 2
+	level_config.characters.append(char_config)
+	char_config = CoreLevelConfig.CharacterConfig.new()
+	char_config.inst_cls = CoreGameplay.CharacterClass.MAGE
+	char_config.inst_pos = Vector2i(5, 0)
+	char_config.inst_face = 0
+	level_config.characters.append(char_config)
+	level_config.boxes = [
+		Vector2i(10, 1),
+	]
+	level_config.pads = [
+		Vector2i(1, 1),
+	]
+	door_config = CoreLevelConfig.DoorConfig.new();
+	door_config.position = Vector2i(7, 1)
+	door_config.need_pads_indices = [0]
+	level_config.doors.append(door_config)
+	level_theme_config = LevelTheme.LevelThemeConfig.new()
+	config_entry = LevelConfigEntry.new()
+	config_entry.name = "LEVEL 16"
+	config_entry.level_config = level_config
+	config_entry.level_theme_config = level_theme_config
+	result.append(config_entry)
+	
+
+	# LEVEL 17
+	level_config = CoreLevelConfig.LevelConfig.new()
+	level_config.obstacles = [
+		Vector2i(1, 0),
+		Vector2i(4, 0),
+		Vector2i(7, 0),
+		Vector2i(1, 1),
+		Vector2i(7, 1),
+		Vector2i(1, 2),
+		Vector2i(4, 2),
+		Vector2i(7, 2),
+		Vector2i(1, 3),
+		Vector2i(2, 3),
+		Vector2i(3, 3),
+		Vector2i(4, 3),
+		Vector2i(5, 3),
+		Vector2i(6, 3),
+		Vector2i(7, 3),
+	]
+	level_config.range = Rect2i(0, 0, 9, 5)
+	level_config.goals = [
+		Vector2i(2, 2),
+		Vector2i(3, 4),
+		Vector2i(4, 4),
+	]
+	level_config.characters = []
+	char_config = CoreLevelConfig.CharacterConfig.new()
+	char_config.inst_cls = CoreGameplay.CharacterClass.WARRIOR
+	char_config.inst_pos = Vector2i(8, 0)
+	char_config.inst_face = 0
+	level_config.characters.append(char_config)
+	char_config = CoreLevelConfig.CharacterConfig.new()
+	char_config.inst_cls = CoreGameplay.CharacterClass.THIEF
+	char_config.inst_pos = Vector2i(0, 0)
+	char_config.inst_face = 0
+	level_config.characters.append(char_config)
+	char_config = CoreLevelConfig.CharacterConfig.new()
+	char_config.inst_cls = CoreGameplay.CharacterClass.MAGE
+	char_config.inst_pos = Vector2i(2, 0)
+	char_config.inst_face = 0
+	level_config.characters.append(char_config)
+	level_config.boxes = [
+		Vector2i(6, 0),
+		Vector2i(8, 1),
+		Vector2i(0, 4),
+		Vector2i(8, 4),
+	]
+	level_config.pads = [
+		Vector2i(3, 1),
+		Vector2i(5, 1),
+	]
+	door_config = CoreLevelConfig.DoorConfig.new();
+	door_config.position = Vector2i(0, 2)
+	door_config.need_pads_indices = [0, 1]
+	level_config.doors.append(door_config)
+	door_config = CoreLevelConfig.DoorConfig.new();
+	door_config.position = Vector2i(8, 2)
+	door_config.need_pads_indices = [0, 1]
+	level_config.doors.append(door_config)
+	level_theme_config = LevelTheme.LevelThemeConfig.new()
+	config_entry = LevelConfigEntry.new()
+	config_entry.name = "LEVEL 17"
+	config_entry.level_config = level_config
+	config_entry.level_theme_config = level_theme_config
+	result.append(config_entry)
+	
 	return result
